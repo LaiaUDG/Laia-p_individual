141ac8b26f5072e76b036b6869972eb5cc5ee083
Laia106 <u1979260@campus.udg.edu>
2023-04-24 15:52:40 +0200

    as

--- pi_4/js/scenes/game.js
+++ pi_4/js/scenes/game.js
@@ -26,34 +26,26 @@ class GameScene extends Phaser.Scene {
 		let arraycards = [];
 		arraycards=this.items.slice();
 		arraycards=Phaser.Utils.Array.Shuffle(arraycards)
-		arraycards = arraycards.slice(0,config.num_cards);
+		arraycards = arraycards.slice(0,config.cards);
+		console.log(config)
+		console.log(config.cards)
 		arraycards = arraycards.concat(arraycards);
 		arraycards=Phaser.Utils.Array.Shuffle(arraycards)
+		console.log(arraycards.length)
 		for (var i = 0; i<arraycards.length; i++){
-			this.current_card.add(this,this.current_card,{done: false, texture: back});
+			Phaser.Utils.Array.Add(this.current_card,{done: false, texture: 'back'});
 		}
 		var mig = 400
-		var x = mig - (config.num_cards * 100) + 50
+		var x = mig - (config.cards * 100) + 50
 		setTimeout(()=> {
 			for (var i = 0; i<arraycards.length; i++){
 				this.add.image(x,300,arraycards[i])
+				this.cards.create(x, 300, 'back');
 				x += 100
 			}
 		},1000)
-		
-		this.cameras.main.setBackgroundColor(0xBFFCFF);
-		this.add.image(250, 300, arraycards[0]);
-		this.add.image(350, 300, arraycards[1]);
-		this.add.image(450, 300, arraycards[2]);
-		this.add.image(550, 300, arraycards[3]);
-		
+		this.cameras.main.setBackgroundColor(0xBFFCFF);		
 		this.cards = this.physics.add.staticGroup();
-		
-		this.cards.create(250, 300, 'back');
-		this.cards.create(350, 300, 'back');
-		this.cards.create(450, 300, 'back');
-		this.cards.create(550, 300, 'back');
-		
 		i = 0;
 		this.cards.children.iterate((card)=>{
 			card.card_id = arraycards[i];
